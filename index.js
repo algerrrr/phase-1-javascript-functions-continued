3297196a3aaadd7ce3345bd0438e2b249f0d3709
Algernon Soriano <alger.soriano@gmail.com>
2023-08-13 19:52:29 +1000

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(act="roller-skate"){
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(act="go to the office"){
+    return `This Monday, I will ${act}.`
+}
+function wrapAdjective(wrapper){
+    return function adjective(msg){
+        return `You are ${wrapper}${msg}${wrapper}!`
+    }
+}
